76b6f48927367cb1e2b5fe4920977c468e6563a7
tvquang0511 <23120346@student.hcmus.edu.vn>
2026-08-29 23:15:36 +0700

    chore: delete gemini service, all service use ai services insteed

--- backend/src/infrastructure/ai/index.ts
+++ backend/src/infrastructure/ai/index.ts
@@ -3,5 +3,6 @@ export * from "./providers/gemini.provider";
 export * from "./providers/groq.provider";
 export * from "./providers/openai.provider";
 export * from "./providers/openrouter.provider";
+export * from "./providers/deepseek.provider";
+export * from "./providers/together.provider";
 export * from "./ai.service";
-export * from "./gemini.service";
